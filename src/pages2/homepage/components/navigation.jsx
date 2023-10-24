f849fc79edb65831c3f5af3d364a8843fe8e8c63
MateusCMartins724 <mateus.c.martins@outlook.com>
2023-10-23 22:25:06 -0300

    add navigation component

---
+++ src/pages2/homepage/components/navigation.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import React from "react";
+import {Activities, Header,Card, CardText, Anchor} from '../styled-components/navigation'
+import SchoolIcon from '@mui/icons-material/School';
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+
+
+export default function NavigationComponent() {
+    return(
+        <>
+        <Activities>See all activities</Activities>
+        <Header>College Information: </Header>
+        <Anchor href="#">
+        <Card>
+            <CardText>
+                College Information
+            </CardText>
+            <SchoolIcon sx={{ fontSize: 72 }} style={{marginRight: "20px"}} ></SchoolIcon>
+        </Card>
+        </Anchor>
+        <Anchor href="#">
+        <Card>
+            <CollectionsBookmarkIcon sx={{ fontSize: 72 }} style={{marginLeft: "35px"}} ></CollectionsBookmarkIcon>
+            <CardText style={{marginLeft: "60px"}}>
+                College Courses
+            </CardText>
+        </Card>
+        </Anchor>
+        <Anchor href="#">
+        <Card>
+            <CardText>
+                Students Organizations
+            </CardText>
+            <PeopleAltIcon sx={{ fontSize: 72 }} style={{marginRight: "20px"}} ></PeopleAltIcon>
+        </Card>
+        </Anchor>
+        </>
+    )
+}
---
+++ src/pages2/homepage/styled-components/navigation.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+
+
+const Activities = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: 120%;
+    width: 90%;
+    margin: auto;
+    margin-top: 20px;
+    background-color: #001C35;
+    text-align: center;
+    color: #f6f6f6;
+    padding: 10px 0;
+    border-radius: 5px;
+    box-shadow: 0 0 4px #001C35;
+`;
+
+const Header = styled.h3`
+    color: #f6f6f6;
+    font-size: 140%;
+    font-weight: normal;
+    margin-left: 10px;
+`;
+
+const Card = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between; 
+    margin: auto;
+    width: 90%;
+    background-color: #014785;
+    box-shadow: 4px 4px 4px 1px #001C35;
+    margin-bottom: 20px;
+    border-radius: 5px;
+    padding: 2%; 
+    position: relative;
+    &:hover{
+        background-color: #001C35;
+        border-radius: 5px;
+        box-shadow: 0 0 4px #001C35;  
+    }
+`;
+
+const CardText = styled.div`
+    margin: 0;
+    font-size: 180%;
+    color: #f6f6f6;
+    padding: 15px 0 15px 0;
+    margin-left: 30px;
+`;
+
+const Anchor = styled.a`
+    text-decoration: none;
+    color: #f6f6f6;
+`;
+
+
+export {Activities, Header, Card, CardText, Anchor}
