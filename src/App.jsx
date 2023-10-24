fc87c4ccf38950c3b93a8d985109c36362d0c7fc
MateusCMartins724 <mateus.c.martins@outlook.com>
2023-10-24 16:10:06 -0300

    change app js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import Courses from './pages/courses/courses';
 
 function App() {
   return (
     <Router>
       <main className="content">
         {/* <Navigation /> */}
-
         <Routes>
           <Route path="/homepage" element={<HomePage />} />
+          <Route path="/courses" element={<Courses />} />
         </Routes>
       </main>
     </Router>
