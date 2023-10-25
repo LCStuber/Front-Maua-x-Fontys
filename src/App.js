e1d4b9bfa67e77facdbbb3620101e17b9697c0a6
yosif <y.yosifov@student.fontys.nl>
2023-10-25 21:37:17 +0200

    navigation bar finished for every page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import React, {useState} from 'react';
+import Navbar from './project-components/navbar';
 
 function App() {
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => {
+    setIsDrawerOpen(open);
+  };
+
   return (
     <Router>
       <main className="content">
         {/* <Navigation /> */}
-
+        <Navbar openDrawer={toggleDrawer}/>
         <Routes>
           <Route path="/homepage" element={<HomePage />} />
         </Routes>
