67c26e03c8af72160295d4ddc85e8935be400006
yosif <y.yosifov@student.fontys.nl>
2023-10-30 13:37:13 +0100

    subscribe button changing colors

--- src/pages/activityDetail/ActivityDetail.js
+++ src/pages/activityDetail/ActivityDetail.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ActivityDetail.css';
 import { useParams } from 'react-router-dom';
 
 const ActivityDetail = () => {
   const { id } = useParams();
 
+  const [isRed, setIsRed] = useState(false);
+
+  const toggleColor = () => {
+    setIsRed(!isRed);
+  }
   console.log(id);
 
   return (
     <div>
       <h2>Activity Detail</h2>
-      <button className="subscribeButton">Subscribe</button>
+      <button
+        className={`subscribeButton ${isRed ? 'red' : 'green'}`}
+        onClick={toggleColor}
+      >
+        Subscribe
+      </button>
       {/* Display detailed activity information here */}
     </div>
   );
