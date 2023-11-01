fd4bd3a6f1110347543c24e7777ac254cb976550
MateusCMartins724 <mateus.c.martins@outlook.com>
2023-10-31 22:15:40 -0300

    change header

---
+++ src/styled-components/Header.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  font-size: 200%;
+  color: #f6f6f6;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  margin-left: 20px;
+  font-weight: normal;
+`;
+
+export {Header};
