3fa3d905f0f78ef154193d46eb8f678eaa9b49ea
aavieux <58111856+aavieux@users.noreply.github.com>
2023-11-08 19:44:28 +0100

    Fixed merge conflicts.

--- src/pages/activities/Activities.js
+++ src/pages/activities/Activities.js
@@ -3,44 +3,6 @@ import api from '../../api/axiosConfig';
 import './Activities.css'
 import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8115a1e (Getting the list of activities from the API.)
-// export const activities = [
-//   {
-//     id: '1',
-//     title: 'Activity 1',
-//     room: 'W102',
-//     time: '10:00 AM',
-//     lector: 'Andreia Machion',
-//     max_capacity: '30',
-//     used_capacity: '3',
-//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-//   },
-//   {
-//     id: '2',
-//     title: 'Activity 2',
-//     room: 'H204',
-//     time: '2:30 PM',
-//     lector: 'Andreia Machion',
-//     max_capacity: '20',
-//     used_capacity: '5',
-//     description: "desctiption of the activity",
-//   },
-//   {
-//     id: '3',
-//     title: 'Activity 3',
-//     room: 'D01',
-//     time: '4:30 PM',
-//     lector: 'Andreia Machion',
-//     max_capacity: '25',
-//     used_capacity: '7',
-//     description: "desctiption of the activity",
-//   },
-//   // Add more activity objects as needed
-// ];
-<<<<<<< HEAD
 
 const Activities = () => {
   const [activities, setActivities] = useState([]);
@@ -58,62 +20,6 @@ const Activities = () => {
   useEffect(() => {
     getActivities();
   }, [])
-=======
-export const activities = [
-  {
-    id: '1',
-    title: 'Activity 1',
-    room: 'W102',
-    time: '10:00 AM',
-    lector: 'Andreia Machion',
-    max_capacity: '30',
-    used_capacity: '3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-  },
-  {
-    id: '2',
-    title: 'Activity 2',
-    room: 'H204',
-    time: '2:30 PM',
-    lector: 'Andreia Machion',
-    max_capacity: '20',
-    used_capacity: '5',
-    description: "desctiption of the activity",
-  },
-  {
-    id: '3',
-    title: 'Activity 3',
-    room: 'D01',
-    time: '4:30 PM',
-    lector: 'Andreia Machion',
-    max_capacity: '25',
-    used_capacity: '7',
-    description: "desctiption of the activity",
-  },
-  // Add more activity objects as needed
-];
-
-const Activities = () => {
->>>>>>> 92a63ce (UI for a specific activity page)
-=======
-
-const Activities = () => {
-  const [activities, setActivities] = useState([]);
-
-  const getActivities = async () => {
-    try {
-      const response = await api.get('/api/v1/activities');
-      console.log(response.data);
-      setActivities(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    getActivities();
-  }, [])
->>>>>>> 8115a1e (Getting the list of activities from the API.)
 
   return (
     <div className="activity-list">
--- src/pages/activityDetail/ActivityDetail.js
+++ src/pages/activityDetail/ActivityDetail.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ActivityDetail.css';
 import { useParams } from 'react-router-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import { activities } from '../activities/Activities';
->>>>>>> 92a63ce (UI for a specific activity page)
-=======
->>>>>>> 8115a1e (Getting the list of activities from the API.)
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
@@ -18,41 +12,12 @@ library.add(faUser, faClock, faBuilding, faUsers);
 const ActivityDetail = () => {
   const { id } = useParams();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8115a1e (Getting the list of activities from the API.)
-  const [activity, setActivity] = useState({
-    id: '',
-    title: '',
-    room: '',
-    time: '',
-    lector: '',
-    max_capacity: '',
-    used_capacity: '',
-    description: '',
-  });
-<<<<<<< HEAD
-
-  // useEffect(() => {
-  //   const findActivityById = activities.find((item) => item.id === id);
-  //   setActivity(findActivityById);
-  // }, [id]);
-=======
   const [activity, setActivity] = useState(null);
 
-  useEffect(() => {
-    const findActivityById = activities.find((item) => item.id === id);
-    setActivity(findActivityById);
-  }, [id]);
->>>>>>> 92a63ce (UI for a specific activity page)
-=======
-
   // useEffect(() => {
   //   const findActivityById = activities.find((item) => item.id === id);
   //   setActivity(findActivityById);
   // }, [id]);
->>>>>>> 8115a1e (Getting the list of activities from the API.)
 
   const [isSubscribed, setIsSubscribed] = useState(false);
 
