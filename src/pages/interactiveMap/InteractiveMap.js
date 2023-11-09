c82ce1a05bc8b092f46db81b0adc9eaf2b86a135
aavieux <58111856+aavieux@users.noreply.github.com>
2023-11-09 01:27:35 +0100

    Finished until Block G with some errors

--- src/pages/interactiveMap/InteractiveMap.js
+++ src/pages/interactiveMap/InteractiveMap.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import SelectBlockButton from './components/SelectBlockButton'
 import "./InteractiveMap.css"
+import { GoogleMap } from '@react-google-maps/api'
+import GoogleMapComponent from './components/GoogleMapComponent'
 
 const InteractiveMap = () => {
     return (
         <div className='selectBlockField'>
             <SelectBlockButton />
+            <GoogleMapComponent/>
         </div>
        
     )
--- src/pages/interactiveMap/components/GoogleMapComponent.js
+++ src/pages/interactiveMap/components/GoogleMapComponent.js
@@ -1,29 +1,60 @@
-import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useEffect } from 'react';
+import jsonData from './JSON.json'
 
-const mapContainerStyle = {
-  width: '100%',
-  height: '400px',
-};
+// const mapContainerStyle = {
+//   width: '100%',
+//   height: '400px',
+// };
 
-const defaultCenter = {
-  lat: -23.647945404052734,
-  lng: -46.57420349121094, // maua location 
-};
+// const defaultCenter = {
+//   lat: -23.647945404052734,
+//   lng: -46.57420349121094, // maua location 
+// };
 
 const GoogleMapComponent = () => {
+  useEffect(() => {
+    let map;
+
+    function initMap() {
+      map = new window.google.maps.Map(document.getElementById("map"), {
+        zoom: 16.8,
+        center: {
+          lng: -46.573049,
+          lat: -23.649001,
+
+         
+        },
+      });
+      // Load GeoJSON data from the prop
+      map.data.addGeoJson(jsonData);
+    }
+
+    window.initMap = initMap;
+
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&callback=initMap&v=weekly`;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
-    <LoadScript
-      googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY"
-    >
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={defaultCenter}
-        zoom={15}
-      >
-        <Marker position={defaultCenter} />
-      </GoogleMap>
-    </LoadScript>
+    // <LoadScript
+    //   googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY"
+    // >
+    //   <GoogleMap
+    //     mapContainerStyle={mapContainerStyle}
+    //     center={defaultCenter}
+    //     zoom={15}
+    //   >
+    //     <Marker position={defaultCenter} />
+    //   </GoogleMap>
+    // </LoadScript>
+    <div id="map" style={{ width: '100%', height: '400px' }}></div>
   );
 };
 
