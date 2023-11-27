55f5a295589e9cd2b7661dfacb8f1625bf2e8520
Toma Milkov <tmilkov@appolica.com>
2023-11-27 14:09:44 +0100

    Added activities swiper custom style.

--- src/pages/activities/Activities.js
+++ src/pages/activities/Activities.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import api from '../../api/axiosConfig';
+import 'swiper/css';
+import "swiper/css/navigation"
 import './Activities.css'
 import { Link } from 'react-router-dom';
 
 import SwiperCore from "swiper";
-import { Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules'
-import 'swiper/css';
-import "swiper/css/navigation"
+import { height } from '@mui/system';
+
 SwiperCore.use([Navigation])
 
 function dayOfWeekAsString(dayIndex) {
@@ -47,7 +49,7 @@ const Activities = () => {
         {Object.keys(activitiesByDay).map((day) => {
           const dayDate = new Date(day)
           const dayName = dayOfWeekAsString(dayDate.getDay());
-          
+
           return (
             <div className="day-activities" key={day}>
               <div className="week-title-container">
@@ -70,44 +72,43 @@ const Activities = () => {
       </div>
 
       <div className="activity-lists-container-mobile">
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        loop={true}
-        navigation={{ clickable: true }}
-      >
-        {Object.keys(activitiesByDay).map((day) => {
-          const dayDate = new Date(day);
-          const dayName = dayOfWeekAsString(dayDate.getDay());
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation={{ clickable: true }}
+        >
+          {Object.keys(activitiesByDay).map((day) => {
+            const dayDate = new Date(day);
+            const dayName = dayOfWeekAsString(dayDate.getDay());
 
-          return (
-            <SwiperSlide key={day}>
-              <div className="day-activities">
-                <div className="week-title-container">
-                  {dayName} - {dayDate.getDate()}/{dayDate.getMonth() + 1}/{dayDate.getFullYear()}
-                </div>
-                <div className="activity-list">
-                  {activitiesByDay[day].map((activity) => (
-                    <Link className="activity" to={`/activity/${activity.id}`} key={activity.id}>
-                      <div className="activity-title">{activity.title}</div>
-                      <div className="activity-details">
-                        <div className="activity-room">
-                          <span className="activity-room-title">Room: </span>
-                          {activity.room}
+            return (
+              <SwiperSlide key={day} className='day-activities-container'>
+                <div className="day-activities">
+                  <div className="week-title-container">
+                    {dayName} - {dayDate.getDate()}/{dayDate.getMonth() + 1}/{dayDate.getFullYear()}
+                  </div>
+                  <div className="activity-list">
+                    {activitiesByDay[day].map((activity) => (
+                      <Link className="activity" to={`/activity/${activity.id}`} key={activity.id}>
+                        <div className="activity-title">{activity.title}</div>
+                        <div className="activity-details">
+                          <div className="activity-room">
+                            <span className="activity-room-title">Room: </span>
+                            {activity.room}
+                          </div>
+                          <div className="activity-time">
+                            <span className="activity-room-time">Time: </span>
+                            {activity.time}
+                          </div>
                         </div>
-                        <div className="activity-time">
-                          <span className="activity-room-time">Time: </span>
-                          {activity.time}
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
+                      </Link>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   )
