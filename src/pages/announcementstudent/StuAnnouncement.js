4b96f0fd8cc1c9036bef7574c76d41b21ee6a566
Kaan <kaanerenmemisoglu@gmail.com>
2023-11-30 23:28:19 +0100

    minor fix (front)

--- src/pages/announcementstudent/StuAnnouncement.js
+++ src/pages/announcementstudent/StuAnnouncement.js
@@ -6,7 +6,7 @@ const StuAnnouncement = () => {
     return(
         <>
         <div className="containerStuAnnounce">
-            <h1> Announcements:</h1>
+            <h1 id="announcementtitle"> Announcements:</h1>
             <div className="announcementWrapper">
                 
                 <div className="announcementContainer">
@@ -16,7 +16,14 @@ const StuAnnouncement = () => {
                     </div>
                     <h4> Subject </h4>
                     <p> 
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                     </p>
                 </div>
                 <div className="announcementContainer">
@@ -26,7 +33,7 @@ const StuAnnouncement = () => {
                     </div>
                     <h4> Subject </h4>
                     <p> 
-                        message will be heresd f;lgks;dl f gksd;l fgk sd';l fgks'; dlfgksd;'lfgks';ldfkgs'; dlfgks' d;lfgksd';lfksd';l fgks d';lfgksd';lfgk's;dlkf g';sldfkg';sldkfg';lsdk fg';l sdkfg;'lsdkfg; l'k'dfsg;lkj sd fsdglkj sdfg ;lkjsd  sa'dl;f gf;lk dsfg;l sdfl;l p pfg jerp gj sd;fl sdlfkg g  lkjwe ;l gkjdsf glj gewlj
+                        message will be here
                     </p>
                 </div>
                 <div className="announcementContainer">
@@ -36,7 +43,7 @@ const StuAnnouncement = () => {
                     </div>
                     <h4> Subject </h4>
                     <p> 
-                        message will be heresd f;lgks;dl f gksd;l fgk sd';l fgks'; dlfgksd;'lfgks';ldfkgs'; dlfgks' d;lfgksd';lfksd';l fgks d';lfgksd';lfgk's;dlkf g';sldfkg';sldkfg';lsdk fg';l sdkfg;'lsdkfg; l'k'dfsg;lkj sd fsdglkj sdfg ;lkjsd  sa'dl;f gf;lk dsfg;l sdfl;l p pfg jerp gj sd;fl sdlfkg g  lkjwe ;l gkjdsf glj gewlj
+                        message will be here then lenght of the message doesn't matter
                     </p>
                 </div>
             </div>
