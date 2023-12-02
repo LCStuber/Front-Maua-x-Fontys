7db525e4e7387c3073d1ba5b90f7ed6fa95cd83e
MateusCMartins724 <mateus.c.martins@outlook.com>
2023-12-02 15:15:57 -0300

    add college info format

--- src/pages/college-info/collegeInfo.jsx
+++ src/pages/college-info/collegeInfo.jsx
@@ -1,49 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import api from '../../api/axiosConfig';
-import {Header} from '../../styled-components/Header';
-import {Description} from './styled-components/Description';
-import Card from './components/Card'
-import Grid from '@mui/material/Unstable_Grid2';
-import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
-
+import React from 'react';
+import Row1 from './components/row1';
+import Row2 from './components/row2';
+import Row3 from './components/row3';
+import Row4 from './components/row4';
+import Row5 from './components/row5';
+import Row6 from './components/row6';
+import Row7 from './components/row7';
 
 export default function CollegeInfo() {
-  const [collegeInfo, setCollegeInfo] = React.useState([]);
-
-  const getCollegeInfo = async () => {
-    try {
-      const response = await api.get('/api/v1/college');
-      console.log(response.data);
-      setCollegeInfo(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    getCollegeInfo();
-  }, [])
-
   return(
-    <Grid container
-    spacing={0}
-    justifyContent="center"
-    style={{margin: "0"}}>
-        <Grid xs={12}>
-            <Header>Sobre a Mauá:</Header>
-        </Grid>
-        <Grid xs={12} sm={6} md={4} lg={3}>
-            <Card name={collegeInfo.rector} linkImg={collegeInfo.rectorImg}/>
-        </Grid>
-        <Grid xs={12} sm={6} md={4} lg={3} sx={{marginBottom: "64px"}}>
-            <Card name={collegeInfo.viceRector} linkImg={collegeInfo.viceRectorImg}/>
-        </Grid>
-        <Grid xs={12}>
-            <Description>
-                {collegeInfo.description}
-                
-            </Description>
-        </Grid>
-    </Grid>
+    <>
+        <Row1/>
+        <Row2/>
+        <Row3/>
+        <Row4/>
+        <Row5/>
+        <Row6/>
+        <Row7/>
+    </>
   );
 }
