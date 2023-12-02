5dc8152e34fad10da89fac5ba01a01af5f15be43
MateusCMartins724 <mateus.c.martins@outlook.com>
2023-12-02 15:16:08 -0300

    adjust card format

--- src/pages/college-info/components/Card.jsx
+++ src/pages/college-info/components/Card.jsx
@@ -4,18 +4,21 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-export default function ImgMediaCard({linkImg, name}) {
+export default function ImgMediaCard({linkImg, name, cargo}) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 280 }}>
       <CardMedia
         component="img"
-        alt="green iguana"
-        height="140"
+        alt={"Imagem do" + name + "da Mauá"}
+        height="300"
         image={linkImg}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
         {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        {cargo}
         </Typography>
         </CardContent>
     </Card>
