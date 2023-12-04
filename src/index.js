a8eed5a620528c6956102fce4c7bd27da8947cb3
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-12-03 23:53:11 -0300

    Feature: Microsoft Integration

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <MsalProvider instance={msalInstance}>
+          <App />
+      </MsalProvider>
   </React.StrictMode>
 );
 
