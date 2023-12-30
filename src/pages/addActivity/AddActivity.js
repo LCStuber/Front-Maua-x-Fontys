1614f31e4c5e1756efc3f7be305878d648c8c696
yosif <y.yosifov@student.fontys.nl>
2023-12-30 03:25:23 +0200

    add activity page - datepicker for start and end date, fixed imput boxes for some of the fields

--- src/pages/addActivity/AddActivity.js
+++ src/pages/addActivity/AddActivity.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './AddActivity.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const AddActivity = () => {
 
+    const [endDate, setEndDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(new Date());
 
     return (
         <div className="page-container">
@@ -11,24 +17,44 @@ const AddActivity = () => {
             </div>
             <div className="main-container">
                 <div className="add-activity-name">
+                    <input name="name" type="text" placeholder="Name" required/>
                 </div>
 
                 <div className="add-activity-description">
+                    <textarea name="description" type="text" placeholder="Description" rows="4" required/>
                 </div>
 
                 <div className="add-activity-start-date">
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            selected={startDate}
+                            onChange={date => setStartDate(date)}
+                            className="datepicker"
+                            label="Start date"
+                        />
+                    </LocalizationProvider>
                 </div>
 
                 <div className="add-activity-end-date">
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            selected={endDate}
+                            onChange={date => setEndDate(date)}
+                            className="datepicker"
+                            label="End date"
+                        />
+                    </LocalizationProvider>
                 </div>
 
                 <div className="add-activity-lector">
+                    <input name="lector" type="text" placeholder="Lector" required/>
                 </div>
 
                 <div className="add-activity-building">
                 </div>
 
                 <div className="add-activity-room">
+                    <input name="room" type="text" placeholder="Room" required/>
                 </div>
 
                 <div className="add-activity-capacity">
