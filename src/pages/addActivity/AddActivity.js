f22a2067f819b6b2e8bedf25ee74ad902b16bde3
yosif <y.yosifov@student.fontys.nl>
2024-01-21 14:56:03 +0100

    Add activity page connected with API

--- src/pages/addActivity/AddActivity.js
+++ src/pages/addActivity/AddActivity.js
@@ -1,25 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './AddActivity.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { CheckBox } from '@mui/icons-material';
+import api from '../../api/axiosConfig';
 
 const AddActivity = () => {
 
-    const [endDate, setEndDate] = useState(new Date());
-    const [startDate, setStartDate] = useState(new Date());
-    const [building, setBuilding] = useState(null);
-    const [isMandatory, setIsMandatory] = useState(false);
+    const [formData, setFormData] = useState({
+        isMandatory: false,
+        startDate: new Date(),
+        endDate: new Date(),
+        name: '',
+        description: '',
+        room: '',
+        building: 'Block A',
+        capacity: null,
+        lector: ''
+    })
 
-    const handleIsMandatoryChange = () => {
-        if (isMandatory){
-            setIsMandatory(false);
+    const handleChange = (event) => {
+        const { name, value, type, checked } = event.target;
+
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: type === 'checkbox' ? checked : value,
+        }));
+    }
+
+    const handleDateChange = (date, field) => {
+        if (date && date.toDate instanceof Function) {
+            const jsDate = date.toDate();
+            setFormData((prevData) => ({
+                ...prevData,
+                [field]: jsDate,
+            }));
+        } else {
+            console.error("Invalid date object");
         }
-        else{
-            setIsMandatory(true);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        if (formData.startDate instanceof Date && formData.endDate instanceof Date) {
+            const formattedData = {
+                ...formData,
+                startDate: formData.startDate.toISOString(),
+                endDate: formData.endDate.toISOString(),
+            };
+    
+            try{
+                const response = await api.post(`/api/v1/activities`, formattedData);
+                console.log(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            console.error("Invalid startDate or endDate");
         }
+    }
+
+    const handleBuildingChange = (event) => {
+        const { value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            building: value,
+        }));
     }
 
     return (
@@ -29,18 +77,18 @@ const AddActivity = () => {
             </div>
             <div className="main-container">
                 <div className="add-activity-name">
-                    <input name="name" type="text" placeholder="Name" required/>
+                    <input name="name" type="text" placeholder="Name" value={formData.name} onChange={handleChange} required/>
                 </div>
 
                 <div className="add-activity-description">
-                    <textarea name="description" type="text" placeholder="Description" rows="4" required/>
+                    <textarea name="description" type="text" placeholder="Description" rows="4" value={formData.description} onChange={handleChange} required/>
                 </div>
 
                 <div className="add-activity-start-date">
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DatePicker
-                            selected={startDate}
-                            onChange={date => setStartDate(date)}
+                            selected={formData.startDate}
+                            onChange={(date) => handleDateChange(date, 'startDate')}
                             className="datepicker"
                             label="Start date"
                         />
@@ -50,8 +98,8 @@ const AddActivity = () => {
                 <div className="add-activity-end-date">
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DatePicker
-                            selected={endDate}
-                            onChange={date => setEndDate(date)}
+                            selected={formData.endDate}
+                            onChange={(date) => handleDateChange(date, 'endDate')}
                             className="datepicker"
                             label="End date"
                         />
@@ -59,11 +107,11 @@ const AddActivity = () => {
                 </div>
 
                 <div className="add-activity-lector">
-                    <input name="lector" type="text" placeholder="Lector" required/>
+                    <input name="lector" type="text" placeholder="Lector" valye={formData.lector} onChange={handleChange} required/>
                 </div>
 
                 <div className="add-activity-building">
-                    <select className="building-dropdown" value={building} onChange={e => setBuilding(e.target.value)}>
+                    <select className="building-dropdown" value={formData.building} onChange={handleBuildingChange}>
                         <option>Block A</option>
                         <option>Block B</option>
                         <option>Block C</option>
@@ -91,19 +139,19 @@ const AddActivity = () => {
                 </div>
 
                 <div className="add-activity-room">
-                    <input name="room" type="text" placeholder="Room" required/>
+                    <input name="room" type="text" placeholder="Room" value={formData.room} onChange={handleChange} required/>
                 </div>
 
                 <div className="add-activity-capacity">
-                    <input name="capacity" type="number" placeholder="Capacity (not required)" required></input>
+                    <input name="capacity" type="number" placeholder="Capacity (not required)" value={formData.capacity} onChange={handleChange}></input>
                 </div>
 
                 <div className="add-activity-is-mandatory">
                     <p>Is the activity mandatory?</p>
-                    <input name="isMandatory" type="checkbox" value={isMandatory} onChange={() => handleIsMandatoryChange()} />
+                    <input name="isMandatory" type="checkbox" value={formData.isMandatory} onChange={handleChange} />
                 </div>
 
-                <button className="add-activity-submit-button">
+                <button className="add-activity-submit-button" onClick={handleSubmit}>
                     Create activity
                 </button>
             </div>
